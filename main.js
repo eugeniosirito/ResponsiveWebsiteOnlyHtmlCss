209a93286a5ee35c906cca1e96fd36b45b5f20ca
EugenioSirito <eugenio_sirito@hotmail.com>
2022-04-20 19:09:32 -0300

    version 2.0

---
+++ main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('#hero', {delay: 400});
+ScrollReveal().reveal('#we-are', {delay: 400});
+ScrollReveal().reveal('#our-classes', {delay: 400});
+ScrollReveal().reveal('#features', {delay: 400});
+ScrollReveal().reveal('#hero2', {delay: 400});
